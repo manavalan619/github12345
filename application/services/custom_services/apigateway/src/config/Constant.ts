7a5ec41953c831151fd7dd7beee909e9838e8cd0
manavalan619 <manavalan@10decoders.in>
2020-01-22 14:51:10 +0530

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://github12345-app.github12345.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://github12345-app.github12345.svc.cluster.local:3009';
+export const ADMINURL = 'http://github12345-app.github12345.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://github12345-app.github12345.svc.cluster.local:3008';
